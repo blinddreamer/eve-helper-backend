d8f3c6f97076d957002cca44d4ccede20c9d4f01
Vladislav <vladski.angelov@gmail.com>
2025-03-06 00:59:55 +0200

    add flyway config

---
+++ src/main/java/com/example/pandatribe/configs/FlywayConfig.java
@@ -0,0 +1,14 @@
+package com.example.pandatribe.configs;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig implements FlywayMigrationStrategy {
+    @Override
+    public void migrate(Flyway flyway) {
+        flyway.repair();
+        flyway.migrate();
+    }
+}
