347dc528e6f5ddc63bf1e94a5429c1baf6aeb389
Vladislav <vladski.angelov@gmail.com>
2025-03-06 01:10:10 +0200

    updated flyway migration script

--- src/main/resources/db/migration/V9_2025.03.06__RC1_add_indexes.sql
+++ src/main/resources/db/migration/V9_2025.03.06__RC1_add_indexes.sql
@@ -1,4 +1,4 @@
 CREATE INDEX IF NOT EXISTS industryactivityproducts_activityID_IDX USING BTREE ON industryActivityProducts (activityID);
-CREATE INDEX IF NOT EXISTS invtypes_typeName_IDX USING BTREE ON invtypes (typeName);
-CREATE INDEX IF NOT EXISTS mapsolarsystems_solarSystemName_IDX USING BTREE ON mapsolarsystems (solarSystemName);
+CREATE INDEX IF NOT EXISTS invtypes_typeName_IDX USING BTREE ON invTypes (typeName);
+CREATE INDEX IF NOT EXISTS mapsolarsystems_solarSystemName_IDX USING BTREE ON mapSolarSystems (solarSystemName);
 
