e88eb8c3c035de2b3ced4cdccae79fc7d071ae60
Vladislav <vladski.angelov@gmail.com>
2025-03-09 02:57:17 +0200

    added logger for scheduled removal jobs

--- src/main/java/com/example/pandatribe/scheduledjobs/ScheduledJobsService.java
+++ src/main/java/com/example/pandatribe/scheduledjobs/ScheduledJobsService.java
@@ -5,6 +5,7 @@ import com.example.pandatribe.models.BlueprintData;
 import com.example.pandatribe.repositories.interfaces.AppraisalDataRepository;
 import com.example.pandatribe.repositories.interfaces.BlueprintDataRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class ScheduledJobsService {
     public static final int OLDER_THAN_DAYS = 7;
     public static final String RUN_EVERY_SUNDAY_AT_4_AM ="0 0 4 * * SUN";
@@ -34,6 +36,7 @@ public class ScheduledJobsService {
         Date olderThanSevenDays = Date.from(tempDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
         List<BlueprintData> blueprintsToRemove = blueprintDataRepository.findByCreationDateBefore(olderThanSevenDays);
         blueprintDataRepository.deleteAll(blueprintsToRemove);
+        log.info("Removed " + blueprintsToRemove.size() + " blueprint data");
     }
 
     private void scheduledRemoveAppraisals(){
@@ -42,5 +45,6 @@ public class ScheduledJobsService {
         Date olderThanSevenDays = Date.from(tempDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
         List<AppraisalData> appraisalsToRemove = appraisalDataRepository.findAppraisalDataByCreationDateBefore(olderThanSevenDays);
         appraisalDataRepository.deleteAll(appraisalsToRemove);
+        log.info("Removed " + appraisalsToRemove.size() + " appraisal data");
     }
 }
