bd7d0b1975f221349f14483f42922a2af072f663
Vladislav <vladski.angelov@gmail.com>
2025-03-09 02:55:04 +0200

    added scheduled removals for older appraisals and blueprint searches.

--- src/main/java/com/example/pandatribe/scheduledjobs/ScheduledJobsService.java
+++ src/main/java/com/example/pandatribe/scheduledjobs/ScheduledJobsService.java
@@ -16,10 +16,13 @@ import java.util.List;
 @AllArgsConstructor
 @Service
 public class ScheduledJobsService {
+    public static final int OLDER_THAN_DAYS = 7;
+    public static final String RUN_EVERY_SUNDAY_AT_4_AM ="0 0 4 * * SUN";
+
     private final AppraisalDataRepository appraisalDataRepository;
     private final BlueprintDataRepository blueprintDataRepository;
 
-    @Scheduled(cron = "0 0 4 * * SUN", zone = "UTC")
+    @Scheduled(cron = RUN_EVERY_SUNDAY_AT_4_AM, zone = "UTC")
     public void runWeeklyTask(){
         scheduledRemoveAppraisals();
         scheduledRemoveBlueprintData();
@@ -27,7 +30,7 @@ public class ScheduledJobsService {
 
     private void scheduledRemoveBlueprintData(){
         LocalDate currentDate = LocalDate.now();
-        LocalDate tempDate = currentDate.minusDays(7);
+        LocalDate tempDate = currentDate.minusDays(OLDER_THAN_DAYS);
         Date olderThanSevenDays = Date.from(tempDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
         List<BlueprintData> blueprintsToRemove = blueprintDataRepository.findByCreationDateBefore(olderThanSevenDays);
         blueprintDataRepository.deleteAll(blueprintsToRemove);
@@ -35,7 +38,7 @@ public class ScheduledJobsService {
 
     private void scheduledRemoveAppraisals(){
         LocalDate currentDate = LocalDate.now();
-        LocalDate tempDate = currentDate.minusDays(7);
+        LocalDate tempDate = currentDate.minusDays(OLDER_THAN_DAYS);
         Date olderThanSevenDays = Date.from(tempDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
         List<AppraisalData> appraisalsToRemove = appraisalDataRepository.findAppraisalDataByCreationDateBefore(olderThanSevenDays);
         appraisalDataRepository.deleteAll(appraisalsToRemove);
