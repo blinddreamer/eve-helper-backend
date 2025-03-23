3173ec70e50108526cd68717677a1467fd03b1c6
Vladislav <vladski.angelov@gmail.com>
2025-03-23 12:29:15 +0200

    update eve sde with new dumps

--- src/main/resources/db/migration/V9_2025.03.06__RC1_add_indexes.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX IF NOT EXISTS industryactivityproducts_activityID_IDX USING BTREE ON industryActivityProducts (activityID);
-CREATE INDEX IF NOT EXISTS invtypes_typeName_IDX USING BTREE ON invTypes (typeName);
-CREATE INDEX IF NOT EXISTS mapsolarsystems_solarSystemName_IDX USING BTREE ON mapSolarSystems (solarSystemName);
-
